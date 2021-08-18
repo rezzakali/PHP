b6033de4216d520c350f158086a05fbad47d74e7
rezzakali <myrezzakalirk@gmail.com>
2021-08-18 15:53:55 +0530

    first commit

--- PHPTest.php
+++ PHPTest.php
@@ -1,20 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        Name: <input type="text" name="name">
+        <input type="submit">
+    </form>
+
 <?php
 
-
-echo $_SERVER['PHP_SELF'];
-echo "<br>";
-echo $_SERVER['SERVER_NAME'];
-echo "<br>";
-echo $_SERVER['HTTP_HOST'];
-echo "<br>";
-echo $_SERVER['HTTP_REFERER'];
-echo "<br>";
-echo $_SERVER['HTTP_USER_AGENT'];
-echo "<br>";
-echo $_SERVER['SCRIPT_NAME'];
-
-
-
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $name=$_REQUEST['name'];
+    if(empty($name)){
+        echo "Name is empty";
+    }else{
+        echo $name;
+    }
+}
 
 ?>
 
+
+</body>
+</html>
